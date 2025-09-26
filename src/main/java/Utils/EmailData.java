65a4d6de1d726ad5525a96d8dc5f30c1782c678b
Sushant8292 <sushj2405@gmail.com>
2025-09-26 17:01:15 +0530

    List of files and Sushant profile added
    List of files and Sushant profile added

--- src/main/java/Utils/EmailData.java
+++ src/main/java/Utils/EmailData.java
@@ -34,5 +34,39 @@ public class EmailData {
             "Raju Godamwar\n" +
             "rajugodamwar@gmail.com\n" +
             "8625883565\n";
+    
+    
+    public static String SUSHANT_EMAIL_SUBJECT = "Application for SDET| 3.10 YOE|Immediate Joiner|7798748292";
+    public static String SUSHANT_EMAIL_BODY = "Hello,\n" +
+            "Greetings of the day!\n" +
+            "\n" +
+            "My name is Sushant Jadhav, and I have 3.10 years of experience as a QA Automation Test Engineer at RMES India Pvt. Ltd. I am writing to express my interest in a Manual & QA Automation Test Engineer position at your organization. If there are any suitable openings, I would be grateful for the opportunity to be considered.\n" +
+            "\n" +
+            "Below are my skill sets and professional details for your reference:\n" +
+            "\n" +
+            "Skills:\n" +
+            "\n" +
+            "✔ Automation Testing: Selenium WebDriver,Playwright with Java, TestNG, Maven, Page Object Model (POM), Locating Strategies\n" +
+            "✔ Manual Testing: Functional Testing, Regression Testing, Smoke Testing, API Testing\n" +
+            "✔ Programming & Scripting: Java, OOP Concepts, Unix Scripting, Collections, Exception Handling, Git commands\n" +
+            "✔ API Testing & Tools: Postman, REST API Validation\n" +
+            "✔ CI/CD & Version Control: Jenkins, Git, Bitbucket\n" +
+            "✔ Databases: PostgreSQL, SQL\n" +
+            "✔ Defect Management & Methodologies: JIRA, Test Planning, Agile Scrum\n" +
+            "\n" +
+            "I have successfully led test planning activities, developed automation frameworks, and implemented test strategies that have improved test coverage and reduced manual effort. My collaborative approach with cross-functional teams has resulted in increased efficiency and faster release cycles.\n" +
+            "\n" +
+            "Notice Period: Serving notice period\n" +
+            "Current CTC: 11.0 LPA\n" +
+            "Total Experience: 3.10 years\n" +
+            "Current Location: Hyderabad\n" +
+            "\n" +
+            "Please find my CV attached for your reference. I look forward to any opportunities you may have.\n" +
+            "\n" +
+            "Thanks & Regards,\n" +
+            "Sushant Jadhav\n" +
+            "sushj2405@gmail.com\n" +
+            "7798748292\n";
+
 }
 
--- src/test/java/org/com/qa/NaukriTest/SendEmailTest.java
+++ src/test/java/org/com/qa/NaukriTest/SendEmailTest.java
@@ -1,21 +1,42 @@
 package org.com.qa.NaukriTest;
 
-import Utils.Constant;
-import Utils.EmailUtils;
-import Utils.ExtractHREmailsFromPDF;
-import org.testng.annotations.Test;
-
-import java.util.Set;
-
 import static Utils.Constant.HYD_HR_EMAIL_PATH;
+import static Utils.Constant.PUNE_HR_EMAIL_PATH;
+import static Utils.Constant.BANG_HR_EMAIL_PATH;
+import static Utils.Constant.TestingJobs_HR_EMAIL_PATH;
 import static Utils.Constant.RAJU_RESUME_PATH;
+import static Utils.Constant.SUSHANT_RESUME_PATH;
+import static Utils.Constant.WFH_HR_EMAIL_PATH;
 import static Utils.EmailData.RAJU_EMAIL_BODY;
 import static Utils.EmailData.RAJU_EMAIL_SUBJECT;
+import static Utils.EmailData.SUSHANT_EMAIL_BODY;
+import static Utils.EmailData.SUSHANT_EMAIL_SUBJECT;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.testng.annotations.Test;
+
+import Utils.EmailUtils;
+import Utils.ExtractHREmailsFromPDF;
 
 
 public class SendEmailTest {
+	
+public ArrayList <String>  allHrEmails() {
+     ArrayList <String> files = new ArrayList<String>();
+     files.add(WFH_HR_EMAIL_PATH);
+     files.add(PUNE_HR_EMAIL_PATH);
+     files.add(HYD_HR_EMAIL_PATH);
+     files.add(BANG_HR_EMAIL_PATH);
+     files.add(TestingJobs_HR_EMAIL_PATH);
+     return files;
+}
+
+	
     @Test
     public void sendEmail() {
+    	
         ExtractHREmailsFromPDF extractHREmailsFromPDF = new ExtractHREmailsFromPDF();
         Set<String> emails = extractHREmailsFromPDF.extractEmails(HYD_HR_EMAIL_PATH);
         EmailUtils emailUtils = new EmailUtils();
@@ -23,5 +44,19 @@ public class SendEmailTest {
             emailUtils.sendEmail("rajugodamwar@gmail.com","phqsuaaykrdufiav",email, RAJU_EMAIL_SUBJECT ,RAJU_EMAIL_BODY,RAJU_RESUME_PATH);
         }
         System.out.println("Total Email Sent to HR is :"+emails.size());
+    }
+    
+    @Test
+    public void sendEmailSushant() {
+        ExtractHREmailsFromPDF extractHREmailsFromPDF = new ExtractHREmailsFromPDF();
+    	 ArrayList <String> filesPath =allHrEmails();
+        for (String file : filesPath) {
+		        Set<String> emails = extractHREmailsFromPDF.extractEmails(file);
+		        EmailUtils emailUtils = new EmailUtils();
+		        for(String email: emails){
+		            emailUtils.sendEmail("sushj2405@gmail.com","khejraffnvojznnb",email, SUSHANT_EMAIL_SUBJECT ,SUSHANT_EMAIL_BODY,SUSHANT_RESUME_PATH);
+		        }
+		        System.out.println("Total Email Sent to HR is :"+emails.size());
+		        }
     }
 }
